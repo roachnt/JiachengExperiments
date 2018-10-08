30dc0b982348829ef6b1f5051d19c4b85435600f
Nick Roach <roachnt@miamioh.edu>
2018-10-08 14:19:15 -0400

    sort results

--- deriv_central_Runner.py
+++ deriv_central_Runner.py
@@ -2,6 +2,6 @@ import os
 
 for i in range(1, 11):
     for prob in [25, 50, 75]:
-        os.system("python deriv_centralESP.py " + str(prob) + " " + str(i))
+        os.system("python deriv_centralESP.1.py " + str(prob) + " " + str(i))
 
-        os.system("python deriv_centralFL.py " + str(prob) + " " + str(i))
+        os.system("python deriv_centralFL.1.py " + str(prob) + " " + str(i))
